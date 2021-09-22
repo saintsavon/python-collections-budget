69542bead2d0894d4579b527dcf8ad85e27149a2
saintsavon <jaylensavon@gmail.com>
2021-09-22 00:25:11 -0400

    Added timeit call

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -17,6 +17,8 @@ def main():
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
 
+    timeit.timeit()
+
     print(timeit.timeit(stmt = "expenses.categorize_for_loop()", setup = '''from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
