0248e5f772c1e461889e3d886e8d75489a0f731a
saintsavon <jaylensavon@gmail.com>
2021-09-21 22:15:55 -0400

    Stupid indentation *facepalm*

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,4 @@
-from . import Expense
+import Expense
 
 class BudgetList():
 
@@ -12,10 +12,10 @@ class BudgetList():
     def append(self, item):
         if (self.sum_expenses+item < self.budget):
             self.expenses.append(item)
-            self.sum_expenses += item
+            self.sum_expenses+=item
         else:
             self.overages.append(item)
-            self.sum_overages += item
+            self.sum_overages+=item
         
     def __len__ (self):
         return (len(self.expenses) + len(self.overages))
@@ -29,7 +29,7 @@ def main():
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
 
-        print("The count of all expenses: " + str(len(myBudgetList)))
+    print("The count of all expenses: " + str(len(myBudgetList)))
 
 if __name__ == "__main__":
     main()
